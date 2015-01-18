e26b0b1519bf0cf188687f51e1ea81201e7612d8
Alexey Dmitriev <riad@yandex-team.ru>
2015-01-19 01:16:55 +0300

    Adopts CHelper chrome parser
    + Updates CHerler
    
    Closes #63

---
+++ src/name/admitriev/jhelper/components/ChromeParser.java
@@ -0,0 +1,121 @@
+package name.admitriev.jhelper.components;
+
+import com.intellij.notification.NotificationType;
+import com.intellij.openapi.components.AbstractProjectComponent;
+import com.intellij.openapi.project.Project;
+import com.intellij.util.Consumer;
+import com.intellij.util.text.StringTokenizer;
+import name.admitriev.jhelper.IDEUtils;
+import name.admitriev.jhelper.network.SimpleHttpServer;
+import name.admitriev.jhelper.task.Task;
+import name.admitriev.jhelper.task.TaskUtils;
+import name.admitriev.jhelper.ui.Notificator;
+import net.egork.chelper.parser.BayanParser;
+import net.egork.chelper.parser.CodeforcesParser;
+import net.egork.chelper.parser.FacebookParser;
+import net.egork.chelper.parser.GCJParser;
+import net.egork.chelper.parser.HackerRankParser;
+import net.egork.chelper.parser.Parser;
+import net.egork.chelper.parser.UsacoParser;
+import net.egork.chelper.parser.YandexParser;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A Component to monitor request from CHelper Chrome Extension and parse them to Tasks
+ */
+public class ChromeParser extends AbstractProjectComponent {
+	private static final int PORT = 4243;
+	private static final Map<String, Parser> PARSERS;
+
+	static {
+		Map<String, Parser> taskParsers = new HashMap<String, Parser>();
+		taskParsers.put("yandex", new YandexParser());
+		taskParsers.put("codeforces", new CodeforcesParser());
+		taskParsers.put("hackerrank", new HackerRankParser());
+		taskParsers.put("facebook", new FacebookParser());
+		taskParsers.put("usaco", new UsacoParser());
+		taskParsers.put("gcj", new GCJParser());
+		taskParsers.put("bayan", new BayanParser());
+		PARSERS = Collections.unmodifiableMap(taskParsers);
+	}
+
+	private SimpleHttpServer server = null;
+
+	public ChromeParser(Project project) {
+		super(project);
+	}
+
+	@Override
+	public void projectOpened() {
+		try {
+			Configurator configurator = myProject.getComponent(Configurator.class);
+			Configurator.State configuration = configurator.getState();
+
+			final String path = configuration.getTasksDirectory();
+
+			server = new SimpleHttpServer(
+					PORT,
+					new Consumer<String>() {
+						@Override
+						public void consume(String request) {
+							StringTokenizer st = new StringTokenizer(request);
+							String type = st.nextToken();
+							Parser parser = PARSERS.get(type);
+							if (parser == null) {
+								Notificator.showNotification(
+										"Unknown parser",
+										"Parser " + type + " unknown, request ignored",
+										NotificationType.INFORMATION
+								);
+								return;
+							}
+							String page = request.substring(st.getCurrentPosition());
+							Collection<net.egork.chelper.task.Task> tasks = parser.parseTaskFromHTML(page);
+							if (tasks.isEmpty()) {
+								Notificator.showNotification(
+										"Couldn't parse any task",
+										"Maybe format changed?",
+										NotificationType.WARNING
+								);
+							}
+							for (net.egork.chelper.task.Task rawTask : tasks) {
+								Task task = new Task(
+										rawTask.name,
+										rawTask.taskClass,
+										String.format("%s/%s.task", path, rawTask.name),
+										rawTask.input,
+										rawTask.output,
+										rawTask.testType,
+										rawTask.tests
+								);
+								TaskUtils.saveTask(task, myProject);
+							}
+
+							IDEUtils.reloadProjectInCLion(myProject);
+						}
+					}
+			);
+
+			new Thread(server, "ChromeParserThread").start();
+		}
+		catch (IOException ignored) {
+			Notificator.showNotification(
+					"Could not create serverSocket for Chrome parser",
+					"Probably another CHelper or JHelper project is running?",
+					NotificationType.ERROR
+			);
+		}
+	}
+
+	@Override
+	public void projectClosed() {
+		if (server != null) {
+			server.stop();
+		}
+	}
+}
---
+++ src/name/admitriev/jhelper/network/SimpleHttpServer.java
@@ -0,0 +1,95 @@
+package name.admitriev.jhelper.network;
+
+import com.intellij.notification.NotificationType;
+import com.intellij.util.Consumer;
+import name.admitriev.jhelper.ui.Notificator;
+
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Simple HTTP Server.
+ * Passes every request without headers to given Consumer
+ */
+public class SimpleHttpServer implements Runnable {
+	private Consumer<String> consumer;
+	private ServerSocket serverSocket = null;
+
+	public SimpleHttpServer(int port, Consumer<String> consumer) throws IOException {
+		serverSocket = new ServerSocket(port);
+		this.consumer = consumer;
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				if (serverSocket.isClosed()) {
+					return;
+				}
+				Socket socket = serverSocket.accept();
+				try {
+					InputStream inputStream = socket.getInputStream();
+					String request = readFromStream(inputStream);
+					String[] strings = request.split("\n\n", 2);
+
+					//ignore headers
+					if (strings.length < 2) {
+						Notificator.showNotification(
+								"ChromeParser",
+								"Got response without body. Ignore.",
+								NotificationType.INFORMATION
+						);
+						continue;
+					}
+					final String text = strings[1];
+
+					SwingUtilities.invokeLater(
+							new Runnable() {
+								@Override
+								public void run() {
+									consumer.consume(text);
+								}
+							}
+					);
+				}
+				finally {
+					socket.close();
+				}
+			}
+			catch (IOException ignored) {
+			}
+		}
+	}
+
+	private static String readFromStream(InputStream inputStream) throws IOException {
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(inputStream, "UTF-8")
+		);
+		try {
+			StringBuilder builder = new StringBuilder();
+			String line;
+			//noinspection NestedAssignment
+			while ((line = reader.readLine()) != null)
+				builder.append(line).append('\n');
+			return builder.toString();
+		}
+		finally {
+			reader.close();
+		}
+	}
+
+	public void stop() {
+		try {
+			serverSocket.close();
+		}
+		catch (IOException ignored) {
+
+		}
+	}
+}
