2cafd6f5fecdb42c8c688e2dd8ae654fac15c4c7
Alexey Dmitriev <riad@yandex-team.ru>
2015-04-03 00:56:59 +0300

    Make it easier to delete bunch of tasks #79

--- src/name/admitriev/jhelper/actions/DeleteTaskAction.java
+++ src/name/admitriev/jhelper/actions/DeleteTaskAction.java
@@ -3,13 +3,13 @@ package name.admitriev.jhelper.actions;
 import com.intellij.execution.RunManagerEx;
 import com.intellij.execution.RunnerAndConfigurationSettings;
 import com.intellij.execution.configurations.RunConfiguration;
-import com.intellij.execution.impl.RunManagerImpl;
 import com.intellij.notification.NotificationType;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import name.admitriev.jhelper.configuration.TaskConfiguration;
+import name.admitriev.jhelper.exceptions.NotificationException;
 import name.admitriev.jhelper.ui.Notificator;
 
 import java.io.IOException;
@@ -17,54 +17,21 @@ import java.io.IOException;
 public class DeleteTaskAction extends BaseAction {
 	@Override
 	protected void performAction(AnActionEvent e) {
-		final Project project = e.getProject();
-		RunnerAndConfigurationSettings selectedConfiguration =
-				RunManagerImpl.getInstanceImpl(project).getSelectedConfiguration();
+		Project project = e.getProject();
+		if (project == null) {
+			throw new NotificationException("No project found", "Are you in any project?");
+		}
+
+		RunManagerEx runManager = RunManagerEx.getInstanceEx(project);
+		RunnerAndConfigurationSettings selectedConfiguration = runManager.getSelectedConfiguration();
 		if (selectedConfiguration == null) {
 			return;
 		}
 		RunConfiguration configuration = selectedConfiguration.getConfiguration();
 		if (configuration instanceof TaskConfiguration) {
-			TaskConfiguration taskConfiguration = (TaskConfiguration) configuration;
-
-			final String path = taskConfiguration.getTask().getPath();
-			final String className = taskConfiguration.getTask().getClassName();
-
-			ApplicationManager.getApplication().runWriteAction(
-					new Runnable() {
-						@Override
-						public void run() {
-							VirtualFile classFile = project.getBaseDir().findFileByRelativePath(
-									path + "/../" + className + ".cpp"
-							);
-							if (classFile != null) {
-								try {
-									classFile.delete(this);
-								}
-								catch (IOException ignored) {
-									Notificator.showNotification(
-											"Couldn't delete class file",
-											NotificationType.WARNING
-									);
-								}
-							}
-							VirtualFile taskFile = project.getBaseDir().findFileByRelativePath(path);
-							if (taskFile != null) {
-								try {
-									taskFile.delete(this);
-								}
-								catch (IOException ignored) {
-									Notificator.showNotification(
-											"Couldn't delete task file",
-											NotificationType.WARNING
-									);
-								}
-							}
-						}
-					}
-			);
-
-			RunManagerEx.getInstanceEx(project).removeConfiguration(selectedConfiguration);
+			removeFiles(project, (TaskConfiguration) configuration);
+			runManager.removeConfiguration(selectedConfiguration);
+			selectSomeTaskConfiguration(runManager);
 		}
 		else {
 			Notificator.showNotification(
@@ -72,6 +39,55 @@ public class DeleteTaskAction extends BaseAction {
 					"To delete a configuration you should choose it first",
 					NotificationType.WARNING
 			);
+		}
+	}
+
+	private void removeFiles(final Project project, TaskConfiguration taskConfiguration) {
+
+		final String path = taskConfiguration.getTask().getPath();
+		final String className = taskConfiguration.getTask().getClassName();
+
+		ApplicationManager.getApplication().runWriteAction(
+				new Runnable() {
+					@Override
+					public void run() {
+						VirtualFile classFile = project.getBaseDir().findFileByRelativePath(
+								path + "/../" + className + ".cpp"
+						);
+						if (classFile != null) {
+							try {
+								classFile.delete(this);
+							}
+							catch (IOException ignored) {
+								Notificator.showNotification(
+										"Couldn't delete class file",
+										NotificationType.WARNING
+								);
+							}
+						}
+						VirtualFile taskFile = project.getBaseDir().findFileByRelativePath(path);
+						if (taskFile != null) {
+							try {
+								taskFile.delete(this);
+							}
+							catch (IOException ignored) {
+								Notificator.showNotification(
+										"Couldn't delete task file",
+										NotificationType.WARNING
+								);
+							}
+						}
+					}
+				}
+		);
+	}
+
+	private void selectSomeTaskConfiguration(RunManagerEx runManager) {
+		for (RunnerAndConfigurationSettings settings : runManager.getAllSettings()) {
+			if (settings.getConfiguration() instanceof TaskConfiguration) {
+				runManager.setSelectedConfiguration(settings);
+				return;
+			}
 		}
 	}
 }
